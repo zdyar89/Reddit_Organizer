e2c11ad582359dbe29f34089a4bc76610683fd5b
zdyar89 <zdyar89@gmail.com>
2020-01-24 16:19:20 -0500

    Fixed up saved posts

--- pyScrapper/scrapperTest.py
+++ pyScrapper/scrapperTest.py
@@ -2,42 +2,31 @@ import praw
 import pandas as pd
 import datetime as dt
 
+username = input('What is your reddit name?: ')
+password = input('What is your password?: ')
+
 reddit = praw.Reddit(client_id='uNyVpBjMbWzxIQ', \
                      client_secret='DiRSAS3ZJ0gfDNYKZmp4-iwRVxE', \
                      user_agent='redditScraper', \
-                     username ='zakapalooza', \
-                     password='Sam!102589')
+                     username =username, \
+                     password=password)
 
-subreddit = reddit.subreddit('art')
+zmd = reddit.user.me()
 
-zmd = reddit.redditor(name='zakapalooza')
-listgen = zmd.saved
+savedpostsdict = {"saved content url": [], \
+                "saved content title": []}
+savedcommentsdict = {"saved comments": []}
 
-for post in listgen:
-    print(post)
+savedcontent = zmd.saved(limit=50)
 
+for submission in savedcontent:
+    if isinstance(submission, praw.models.Comment):
+        savedcommentsdict["saved comments"].append(submission.body)
+    elif isinstance(submission, praw.models.Comment):
+        savedpostsdict["saved content url"].append(submission.url)
+        savedpostsdict["saved content title"].append(submission.title)
 
-top_subreddit = subreddit.top()
-
-##for submission in subreddit.top():
-   ## print(submission.title, submission.id)
-
-topics_dict = { "title":[], \
-                "score":[], \
-                "id":[], "url":[], 
-                "comms_num": [], \
-                "created": [], \
-                "body":[]
-                }
-
-for submission in top_subreddit:
-    topics_dict["title"].append(submission.title)
-    topics_dict["score"].append(submission.score)
-    topics_dict["id"].append(submission.id)
-    topics_dict["url"].append(submission.url)
-    topics_dict["comms_num"].append(submission.num_comments)
-    topics_dict["created"].append(submission.created)
-    topics_dict["body"].append(submission.selftext)
-
-topics_data = pd.DataFrame(topics_dict)
-print(topics_data)
+present_posts_data= pd.DataFrame(savedpostsdict)
+present_comments_data = pd.DataFrame(savedcommentsdict)
+present_posts_data.to_csv('savedredditposts.csv', index=True)
+present_comments_data.to_csv('savedredditcomments.csv', index=True)
