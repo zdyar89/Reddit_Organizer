1f165f253923bd617b13cfd5b21b81cfd1b56db5
zdyar89 <zdyar89@gmail.com>
2020-03-05 22:06:52 -0500

    Adding PostgreSQL Connection

--- pyScrapper/holder.py
+++ pyScrapper/holder.py
@@ -2,10 +2,24 @@
 import praw
 import pandas as pd
 import csv
+from sqlalchemy import create_engine
+import psycopg2
+import sys
+import io
 
 # Ask for user authentication first
 usernamed = 'zakapalooza' #input("What's your username? ")
 passwrd = 'Sam!102589' #input("What's your password? ")
+
+# PostGreSQL DB Connection
+connection = psycopg2.connet(database='redditSortDB', 
+                            user='postgres', 
+                            passwrd='searcy')
+
+cur = connection.cursor()
+cur.execute('SELECT version()')
+version = cur.fetchone()[0]
+print(version)
 
 # User will need to create an app in reddit and put in the id and secret tokens to use this
 reddit = praw.Reddit(client_id='uNyVpBjMbWzxIQ', \
@@ -17,26 +31,27 @@ reddit = praw.Reddit(client_id='uNyVpBjMbWzxIQ', \
 # Created two dictionaries to store the saved content. One for posts and one for comments since they can't mingle
 #savedpostsdict = {"subreddit": [], \"saved content url":[], \"saved content title":[] }
 
-savedpostsdict = dict.fromkeys(["SubReddit", "Title", "Link"])
+savedpostsdict = {"placeholder" : []} 
 # Scrapes all of the user's own content
 savedcontent = reddit.user.me().saved(limit=None)
 
-count = 0
 # Checks each saved content to see if it is a comment or submitted post instance and appends to respective dictionaries
 for submission in savedcontent:
+    
     if isinstance(submission, praw.models.Submission):
-            savedpostsdict.setdefault(str(submission.subreddit), [])
-            savedpostsdict[str(submission.subreddit)].append(submission.title)
-            count += 1
-        
-for item in sorted(savedpostsdict):
-    print(item, ':', savedpostsdict[item], '\n')
+        sub = str(submission.subreddit)
+        title = str(submission.title) + ' : ' + str(submission.url)
+        if sub not in savedpostsdict:
+            savedpostsdict.update({str(sub) : [title]})
+        else:    
+            savedpostsdict[sub].append(' ' + title)
+            
+
+df = pd.DataFrame.from_dict(savedpostsdict,orient='index').transpose()    
+df.to_csv(r'./dataframe.csv')
+#for item in sorted(savedpostsdict):
+    
+    #print(item, ' : ' , savedpostsdict[item] , '\n')
 
 
-##could use pickle module to store as binary stream for lists
 
-# Formats dictonaries with scraped data accordingly with Pandas
-#present_posts_data = pd.DataFrame(savedpostsdict)
-
-# Ports each dictionary to a .csv file and numbers them in order. .csv files should be in the same folder as this main.py script
-#present_posts_data.to_csv('savedredditposts.csv', index=True)
